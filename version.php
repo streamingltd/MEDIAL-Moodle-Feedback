70076da60288804a102dfd6790fdfa1bdd77e961
Tim Williams <tmw@autotrain.org>
2019-02-15 10:39:42 +0000

    Release version 2018121804

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018090708;
+$plugin->version   = 2018121804;
 $plugin->requires  = 2016052300;
 $plugin->component = 'assignfeedback_helixfeedback';
 $plugin->dependencies = array(
-    'mod_helixmedia' => '2018090708'
+    'mod_helixmedia' => '2018121804'
 );
